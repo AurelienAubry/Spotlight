141c19a024df95d6ce0e9f8b00548b9fc125719a
AurelienAubry <aurelien.aubry.dev@gmail.com>
2020-04-05 15:13:56 +0200

    Polish backend + comments

--- backend/app.py
+++ backend/app.py
@@ -8,55 +8,42 @@ app = Flask(__name__)
 logic = Logic()
 
 
-@app.route('/')
-def test():
-    return "Welcome to Flask!"
-
-"""
-@app.route("/data", methods=["GET"])
-def data():
-    dates, data = logic.get_total_daily_listening_data()
-    return json.dumps({"labels": dates, "data": data})
-
-@app.route("/recap", methods=["GET"])
-def recap():
-    most_listened = logic.get_most_listened_artist()
-    most_listened_name = most_listened.index[0]
-    most_listened_min = int(most_listened['minPlayed'][0])
-    return json.dumps({"unique_song": logic.get_unique_songs(), "listened_min": int(logic.get_total_min_listened()), "most_listened_name": most_listened_name, "most_listened_min": most_listened_min})
-"""
-
 @app.route("/get/top-artists", methods=["GET"])
-def get_top_artist():
-    start_date = request.args.get('startDate')
+def get_top_artists():
+    day_offset = int(request.args.get('dayOffset'))
     count = int(request.args.get('count'))
-    top_artist_df = logic.get_top_artist_df(start_date, count)
-    return json.dumps(top_artist_df.to_json(orient='index'))
+    top_artists_df = logic.get_top_artists_df(day_offset, count)
+    return json.dumps(top_artists_df.to_json(orient='index'))
+
 
 @app.route("/get/top-songs", methods=["GET"])
-def get_top_song():
-    start_date = request.args.get('startDate')
+def get_top_songs():
+    day_offset = int(request.args.get('dayOffset'))
     count = int(request.args.get('count'))
-    top_song_df = logic.get_top_song_df(start_date, count)
-    return json.dumps(top_song_df.to_json(orient='index'))
+    top_songs_df = logic.get_top_songs_df(day_offset, count)
+    return json.dumps(top_songs_df.to_json(orient='index'))
+
 
 @app.route("/get/day-hours", methods=["GET"])
 def get_hours_day_listen():
-    start_date = request.args.get('startDate')
-    hours_streamings_df = logic.get_hours_day_listen(start_date)
+    day_offset = int(request.args.get('dayOffset'))
+    hours_streamings_df = logic.get_hours_day_listen(day_offset)
     return json.dumps(hours_streamings_df.to_json(orient='index'))
+
 
 @app.route("/get/min-listened", methods=["GET"])
 def get_min_listened():
-    start_date = request.args.get('startDate')
-    min_listened = logic.get_min_listened(start_date)
-    return json.dumps({"min_listened": int(min_listened)})
+    day_offset = int(request.args.get('dayOffset'))
+    min_listened = logic.get_min_listened(day_offset)
+    return json.dumps({"min_listened": min_listened})
+
 
 @app.route("/get/tracks-listened", methods=["GET"])
 def get_tracks_listened():
-    start_date = request.args.get('startDate')
-    tracks_listened = logic.get_tracks_listened(start_date)
-    return json.dumps({"tracks_listened": int(tracks_listened)})
+    day_offset = int(request.args.get('dayOffset'))
+    tracks_listened = logic.get_tracks_listened(day_offset)
+    return json.dumps({"tracks_listened": tracks_listened})
+
 
 if __name__ == '__main__':
 
--- backend/logic.py
+++ backend/logic.py
@@ -6,8 +6,13 @@ import pandas as pd
 class Logic:
 
     def create_streaming_df(self):
+        """
+        Create a dataframe from StreamingHistory files, containing all the user's streaming history
+        """
+
         spotify_data_path = 'MyData'
-        stream_hist_files = [os.path.join(spotify_data_path, x) for x in os.listdir(spotify_data_path) if x.split('.')[0][:-1] == 'StreamingHistory']
+        stream_hist_files = [os.path.join(spotify_data_path, x) for x in os.listdir(
+            spotify_data_path) if x.split('.')[0][:-1] == 'StreamingHistory']
 
         streamings = []
 
@@ -17,55 +22,147 @@ class Logic:
                 streamings.extend([streaming for streaming in new_streamings])
 
         streamings_df = pd.DataFrame(streamings)
-        streamings_df.rename(columns = {'artistName':'artist', 'trackName':'track'}, inplace = True)
+        streamings_df.rename(columns={'artistName': 'artist',
+                                      'trackName': 'track'}, inplace=True)
         streamings_df['minPlayed'] = streamings_df['msPlayed'] / 60000
         streamings_df.drop('msPlayed', axis=1, inplace=True)
 
         streamings_df['endTime'] = pd.to_datetime(streamings_df['endTime'])
-        streamings_df.set_index('endTime',inplace=True)
+        streamings_df.set_index('endTime', inplace=True)
 
         streamings_df = streamings_df.sort_index(ascending=False)
 
         self.streamings_df = streamings_df
 
     def create_daily_streaming_df(self):
-        daily_streamings_df = self.streamings_df.resample('D').agg({'track': 'describe', 'artist': 'describe', 'minPlayed': 'sum'})
-        daily_streamings_df.columns = ["_".join(col_name).rstrip('_') for col_name in daily_streamings_df.columns.to_flat_index()]
+        """
+        Create a dataframe that aggregates daily streaming informations
+        """
+
+        daily_streamings_df = self.streamings_df.resample('D').agg(
+            {'track': 'describe', 'artist': 'describe', 'minPlayed': 'sum'})
+        daily_streamings_df.columns = ["_".join(col_name).rstrip(
+            '_') for col_name in daily_streamings_df.columns.to_flat_index()]
         daily_streamings_df.drop('artist_count', axis=1, inplace=True)
         daily_streamings_df = daily_streamings_df.sort_index(ascending=False)
-        daily_streamings_df.rename(columns = {'minPlayed_minPlayed':'minPlayed'}, inplace = True)
+        daily_streamings_df.rename(
+            columns={'minPlayed_minPlayed': 'minPlayed'}, inplace=True)
         daily_streamings_df['cumMinPlayed'] = daily_streamings_df['minPlayed'].cumsum()
         daily_streamings_df['cumTrack_count'] = daily_streamings_df['track_count'].cumsum()
 
+        self.start_date = daily_streamings_df.index.min()
+        print(self.start_date)
+        self.end_date = daily_streamings_df.index.max()
+        print(self.end_date)
         self.daily_streamings_df = daily_streamings_df
 
     def create_artist_songs_df(self):
+        """
+        Create a dataframe that link each song to it's artist
+        """
+
         artist_songs_df = self.streamings_df.reset_index()[['artist', 'track']].copy()
-        artist_songs_df.drop_duplicates(subset =['track', 'artist'],keep = False, inplace = True)
+        artist_songs_df.drop_duplicates(
+            subset=['track', 'artist'], keep=False, inplace=True)
         artist_songs_df = artist_songs_df.sort_values(by='artist', ascending=False)
         artist_songs_df = artist_songs_df.reset_index(drop=True)
 
         self.artist_songs_df = artist_songs_df
 
-    def get_top_artist_df(self, start_date, count):
-        top_artist_df = self.streamings_df[self.streamings_df.index > start_date].groupby(by=['artist']).agg({'minPlayed': 'sum', 'track': 'count'}).sort_values(by='minPlayed',ascending=False).head(count)
-        return top_artist_df
+    def get_date_offset(self, start_date, day_offset):
+        """
+        Apply a day offset to the provided date and return the the date
 
-    def get_top_song_df(self, start_date, count):
-        top_songs_df = self.streamings_df[self.streamings_df.index > start_date].groupby(by=['track']).agg({'minPlayed': 'sum', 'track': 'count'}).sort_values(by='minPlayed', ascending=False).head(count)
+        Arguments:
+            start_date {Timestamp} -- the intial date
+            day_offset {int} -- the day offset to apply
+
+        Returns:
+            str -- the initial date with the day offset applied
+        """
+
+        date = start_date - pd.DateOffset(days=day_offset)
+        return date.strftime('%Y-%m-%d')
+
+    def get_top_artists_df(self, day_offset, count):
+        """
+        Get the dataframe of the [count] top artists over the last [day_offset] days
+
+        Arguments:
+            day_offset {int} -- the duration of the analyzed period (last [day_offset] days)
+            count {int} -- the number of top artists to return
+
+        Returns:
+            dataframe -- the dataframe containing the [count] top artists over the last [day_offset] days
+        """
+
+        date = self.get_date_offset(self.end_date, day_offset)
+        top_artists_df = self.streamings_df[self.streamings_df.index > date].groupby(by=['artist']).agg(
+            {'minPlayed': 'sum', 'track': 'count'}).sort_values(by='minPlayed', ascending=False).head(count)
+        return top_artists_df
+
+    def get_top_songs_df(self, day_offset, count):
+        """
+        Get the dataframe of the [count] top songs over the last [day_offset] days
+        
+        Arguments:
+            day_offset {int} -- the duration of the analyzed period (last [day_offset] days)
+            count {int} -- the number of top songs to return
+        
+        Returns:
+            dataframe -- the dataframe containing the [count] top songs over the last [day_offset] days
+        """
+
+        date = self.get_date_offset(self.end_date, day_offset)
+        top_songs_df = self.streamings_df[self.streamings_df.index > date].groupby(by=['track']).agg(
+            {'minPlayed': 'sum', 'track': 'count'}).sort_values(by='minPlayed', ascending=False).head(count)
         return top_songs_df
 
-    def get_hours_day_listen(self, start_date):
-        hours_streamings_df = self.streamings_df[self.streamings_df.index < start_date].resample('1H').sum()
-        hours_streamings_df = hours_streamings_df.groupby(hours_streamings_df.index.hour).agg(lambda x: x.mean(skipna=False))
+    def get_hours_day_listen(self, day_offset):
+        """
+        Get a summary the average minutes listened every hour of the day
+        
+        Arguments:
+            day_offset {int} -- the duration of the analyzed period (last [day_offset] days)
+        
+        Returns:
+            dataframe -- the dataframe containing the averages minutes listened every hour of the day
+        """
+
+        date = self.get_date_offset(self.end_date, day_offset)
+        hours_streamings_df = self.streamings_df[self.streamings_df.index < date].resample(
+            '1H').sum()
+        hours_streamings_df = hours_streamings_df.groupby(
+            hours_streamings_df.index.hour).agg(lambda x: x.mean(skipna=False))
         return hours_streamings_df
 
-    def get_min_listened(self, start_date):
-        return self.daily_streamings_df.at[start_date, 'cumMinPlayed'][0]
+    def get_min_listened(self, day_offset):
+        """
+        Get the number of minutes listened over the last [day_offset] days
+        
+        Arguments:
+            day_offset {int} -- the duration of the analyzed period (last [day_offset] days)
+        
+        Returns:
+            int -- the number of minutes listened over the last [day_offset] days
+        """
 
-    def get_tracks_listened(self, start_date):
-        return self.daily_streamings_df.at[start_date, 'cumTrack_count'][0]
+        date = self.get_date_offset(self.end_date, day_offset)
+        return self.daily_streamings_df.at[date, 'cumMinPlayed'][0]
 
+    def get_tracks_listened(self, day_offset):
+        """
+        Get the number of tracks listend over the last [day_offset] days
+        
+        Arguments:
+            day_offset {int} -- the duration of the analyzed period (last [day_offset] days)
+        
+        Returns:
+            int -- the number of tracks listened over the last [day_offset] days
+        """
+
+        date = self.get_date_offset(self.end_date, day_offset)
+        return self.daily_streamings_df.at[date, 'cumTrack_count'][0]
 
     def __init__(self):
         self.create_streaming_df()
